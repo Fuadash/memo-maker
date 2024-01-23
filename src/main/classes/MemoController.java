4387cfbff536fc3d9915cec0308dee4ffe5bc89c
Fua <zero-geo@hotmail.co.uk>
2024-01-23 18:17:06 +0000

    created MemoSearcher class, to be implemented later

---
+++ src/main/classes/MemoController.java
@@ -0,0 +1,5 @@
+package main.classes;
+
+// Will (probably) parse user input and then decide which modules to point to
+public class MemoController {
+}
---
+++ src/main/classes/MemoSearcher.java
@@ -0,0 +1,48 @@
+package main.classes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+// Searches and returns memos
+public class MemoSearcher {
+    public static List<Path> searchFilesFor(Path root, String target) {
+        List<Path> matchingFiles = new ArrayList<>();
+
+        // Add all folders under the root folder to a stream
+        try (Stream<Path> monthFolders = Files.list(root)){
+            monthFolders.forEach(monthFolder -> {
+                if(Files.isDirectory(monthFolder)){
+                    // Collect all files for the month that have matching text
+                    try (Stream<Path> memoFiles = Files.list(monthFolder)){
+                        List<Path> filesWithText = memoFiles
+                                .filter(file -> containsTargetText(file, target))
+                                .collect(Collectors.toList());
+                        matchingFiles.addAll(filesWithText);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        //todo: Do stuff
+                    }
+                }
+            });
+        } catch (IOException e){
+            e.printStackTrace();
+            //todo: Do stuff
+        }
+
+        return matchingFiles;
+    }
+
+    private static boolean containsTargetText (Path file, String target) {
+        try (Stream<String> lines = Files.lines(file)) {
+            return lines.anyMatch(line -> line.contains(target));
+        } catch (IOException e) {
+            e.printStackTrace(); //todo: DO STUFF
+            return false;
+        }
+    }
+}
